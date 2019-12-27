2718bf25c07dc7a46209ebc4ee758d9f5c0b5d60
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-27 18:51:58 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,7 +32,7 @@ def can_be_created_in_a_block(args = nil)
   # title == "Home Alone"
   # release_date == 1990
   
-  "If there is no arguments being passed in. Then movie title should be 'Home Alone' and movie release_date should be 1990"
+  #"If there is no arguments being passed in. Then movie title should be 'Home Alone' and movie release_date should be 1990"
   Movie.create do |m|
      m.title = args[:title] 
      m.release_date = args[:release_date]
