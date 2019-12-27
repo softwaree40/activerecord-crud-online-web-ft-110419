03e0309583850e578d19c14058f5ff9022827565
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-27 20:28:01 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -73,10 +73,9 @@ end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  Movie.create(title: "Awesome Flick")
-  Movie.update(:title => "Awesome Flick","Even Awesomer Flick")
-  Movie.save 
-  Movie
+  user = Movie.find_by(title: "Awesome Flick")
+  user.title = "Even Awesomer Flick"
+  user.save
   
 end
 
