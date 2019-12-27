ff0785aba82759d2c095c80bfc2cbb70117c00a8
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-27 20:15:31 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -68,7 +68,7 @@ def can_find_using_where_clause_and_be_sorted
   Movie.where("release_date > '2002'").order(release_date: :desc)
   
   # release date descending
-  __
+  
 end
 
 def can_be_found_updated_and_saved
