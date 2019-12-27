3cd4c8d473a7f2209ac4cc9090e77f7060213861
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-27 19:01:25 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,7 +27,7 @@ def can_be_created_with_a_hash_of_attributes
   
 end
 
-def can_be_created_in_a_block(args = nil)
+def can_be_created_in_a_block(args = { title: "home", release_date: 1990 })
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
